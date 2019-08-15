224c87bc1ae81709569fc4e9420081a9359e3eda
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2019-08-15 20:56:28 +1000

    add passing url params to home page widget and features specs

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,4 +5,14 @@ describe 'home page', type: :feature do
     visit '/'
     expect(page).to have_content 'By selecting this checkbox, you agree that cronuts are better than donuts '
   end
+
+  it 'pass cronuts_url url params to cronut acceptance widget' do
+    visit '/?cronuts_url=https://google.com'
+    expect(page.find('a#cronuts_url')[:href]).to eq('https://google.com')
+  end
+
+  it 'pass cronuts_url url params to cronut acceptance widget' do
+    visit '/?donuts_url=https://yahoo.com'
+    expect(page.find('a#donuts_url')[:href]).to eq('https://yahoo.com')
+  end
 end
